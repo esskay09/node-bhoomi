5c0af59701d0c4f86705e9b74630646dd3105917
supertramp09 <esskay099@gmail.com>
2021-07-30 20:59:55 +0530

    sadasd

--- server.js
+++ server.js
@@ -272,8 +272,8 @@ app.get("/generateOrder/", function (req, res) {
   const amount = req.body.amount * 100
 
   var instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
+    key_id: `${process.env.RAZORPAY_KEY_ID}`,
+    key_secret: `${process.env.RAZORPAY_KEY_SECRET}`,
   });
 
   var options = {
